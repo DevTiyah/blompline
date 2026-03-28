e41d00e085929d9ab5f62ebb31abebbb2b9bf7e3
DevTiyah <devtiyah@gmail.com>
2026-03-28 10:40:08 +0100

    check user in session and added logout function

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,33 @@
 import { Button } from "@/components/ui/button";
 import { 
   RegisterLink, 
-  LoginLink } from "@kinde-oss/kinde-auth-nextjs/components"
+  LoginLink,
+  LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components"
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
-export default function Home() {
+export default async function Home() {
+  // trying to check if the user is authenticated and also getting the user in session
+  const { getUser } = getKindeServerSession();
+  const session = await getUser();
+
   return (
     <div>
       <h1>Hello</h1>
-      <RegisterLink>
-        <Button>Get Started</Button>
-      </RegisterLink>
-      <LoginLink>
-        <Button>Login</Button>
-      </LoginLink>
+      {/* imported the auth from kinde and display it on the frontend */}
+      {session ? (
+        <LogoutLink>
+          <Button>Logout</Button>
+        </LogoutLink>
+      ) : (
+        <div>
+          <RegisterLink>
+            <Button>Get Started</Button>
+          </RegisterLink>
+          <LoginLink>
+            <Button>Login</Button>
+          </LoginLink>
+        </div>
+      )}
     </div>
   );
 }
